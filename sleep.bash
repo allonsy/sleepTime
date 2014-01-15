15c7582fa1ec3b6f323ea2f7e0fd047acb8c8a00
Alec S <linuxbash8@gmail.com>
2014-01-15 17:03:15 -0600

    Updated bash script

--- sleep.bash
+++ sleep.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 warn=$(($2-1))
-echo $warn $1 "* * * env DISPLAY=:0 java -jar /home/alecsnyder/Documents/Java/shutdown.jar " > /home/alecsnyder/Documents/cs162/sleep.cron
+echo $warn $1 "* * * wall < /home/alecsnyder/Documents/git/cs162/sleepTime/warning.txt" > /home/alecsnyder/Documents/cs162/sleep.cron
 #echo $2 $1 "* * * poweroff" >> /home/alecsnyder/Documents/cs162/sleep.cron
 sudo crontab /home/alecsnyder/Documents/cs162/sleep.cron
 exit 0
